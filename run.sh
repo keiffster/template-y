841fb13d5e7554b63c3002638db29ce38ca166d5
keithsterling <keith@keithsterling.com>
2017-09-13 17:15:58 +0100

    Initial commit

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 
 clear
 
-export PYTHONPATH=../../src:../../libs/MetOffer-1.3.2:.
+export PYTHONPATH=./src:../template-y/src:../template-y/libs/MetOffer-1.3.2:.
 
-python3 ../../src/programy/clients/console.py --config ./config.yaml --cformat yaml --logging ./logging.yaml
+python3 ../template-y/src/programy/clients/console.py --config ./config.yaml --cformat yaml --logging ./logging.yaml
 

0e533e6abebf2561dd688a59b0aa31ee15f92166
Keith Sterling <keiffster@gmail.com>
2019-02-14 12:12:17 +0000

    Updated scripts

--- scripts/windows/console.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-CLS
-
-IF NOT DEFINED PYTHONPATH (
-    ECHO PYTHONPATH not set
-) ELSE (
-    python -m programy.clients.events.console.client --config ..\..\config\windows\config.yaml --cformat yaml --logging ..\..\config\windows\logging.yaml
-)
--- scripts/xnix/template-y.sh
+++ scripts/xnix/template-y.sh
@@ -1,12 +1,6 @@
-
 #! /bin/sh
 
 clear
-
-if [[ -z "${PYTHONPATH}" ]]; then
-  echo "PYTHONPATH is undefined"
-  exit
-fi
 
 python3 -m programy.clients.events.console.client --config ../../config/xnix/config.yaml --cformat yaml --logging ../../config/xnix/logging.yaml
 
